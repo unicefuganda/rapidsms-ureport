7207ea73821d62e31598f9cf58cc01ed9c14e089
David McCann <david.a.mccann@gmail.com>
2010-12-02 09:39:23 +0300

    added password change and require login to all ureport views

--- ureport/urls.py
+++ ureport/urls.py
@@ -1,18 +1,19 @@
 from django.conf.urls.defaults import *
 from ureport.views import *
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
-    url(r'^ureport/$', tag_view,name="tag_view"),
-    url(r'^ureport/polls/(?P<type>\w){0,1}/$', polls,{'template':'ureport/partials/all_polls.html'}),
-    url(r'^ureport/polls/freeform/$', polls,{'template':'ureport/partials/freeform_polls.html','type':'t'}),
-    url(r'^ureport/tag_cloud/$', tag_cloud),
-    url(r'^ureport/messaging/$', messaging),
-    url(r'^ureport/message_log/$', message_log),
-    url(r'^ureport/pie_graph/$', pie_graph,name="pie_chart"),
-    url(r'^ureport/histogram/$', histogram,name="histogram"),
-    url(r'^ureport/map/$', map,name="map"),
-    url(r'^ureport/dashboard/$', poll_dashboard,name="poll_dashboard"),
-    url(r'^ureport/add_tag/$', add_drop_word),
-    url(r'^ureport/delete_tag/$', delete_drop_word),
-    url(r'^ureport/show_excluded/$', show_ignored_tags),
+    url(r'^ureport/$', login_required(tag_view),name="tag_view"),
+    url(r'^ureport/polls/(?P<type>\w){0,1}/$', login_required(polls),{'template':'ureport/partials/all_polls.html'}),
+    url(r'^ureport/polls/freeform/$', login_required(polls),{'template':'ureport/partials/freeform_polls.html','type':'t'}),
+    url(r'^ureport/tag_cloud/$', login_required(tag_cloud)),
+    url(r'^ureport/messaging/$', login_required(messaging)),
+    url(r'^ureport/message_log/$', login_required(message_log)),
+    url(r'^ureport/pie_graph/$', login_required(pie_graph),name="pie_chart"),
+    url(r'^ureport/histogram/$', login_required(histogram),name="histogram"),
+    url(r'^ureport/map/$', login_required(map),name="map"),
+    url(r'^ureport/dashboard/$', login_required(poll_dashboard),name="poll_dashboard"),
+    url(r'^ureport/add_tag/$', login_required(add_drop_word)),
+    url(r'^ureport/delete_tag/$', login_required(delete_drop_word)),
+    url(r'^ureport/show_excluded/$', login_required(show_ignored_tags)),
 )
