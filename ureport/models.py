7fffe235ef03cbb71fe39a7478db3995790ce955
David McCann <david.a.mccann@gmail.com>
2011-02-23 12:09:29 +0300

    added sites to MassText object

--- ureport/models.py
+++ ureport/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from poll.models import Poll
 from rapidsms.models import Contact
 from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+from django.contrib.sites.managers import CurrentSiteManager
 
 class IgnoredTags(models.Model):
     poll = models.ForeignKey(Poll)
@@ -11,10 +13,12 @@ class IgnoredTags(models.Model):
         return '%s'%self.name
 
 class MassText(models.Model):
+    sites = models.ManyToManyField(Site)
     contacts = models.ManyToManyField(Contact, related_name='masstexts')
     user = models.ForeignKey(User)
     date = models.DateTimeField(auto_now_add=True,null=True)
     text = models.TextField()
+    objects = (CurrentSiteManager('sites') if settings.SITE_ID else models.Manager())
     
     class Meta:
         permissions = (
