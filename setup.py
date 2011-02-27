26f7c53ed3ec8fc23a41889d45669625515393da
David McCann <david.a.mccann@gmail.com>
2011-02-27 14:53:49 +0300

    added setup

---
+++ setup.py
@@ -0,0 +1,41 @@
+from distutils.core import setup
+
+setup(
+    name='rapidsms-ureport',
+    version='0.1',
+    license="BSD",
+
+    requires = [
+        "rapidsms (>=0.9.6a)",
+        'django-eav',
+        'rapidsms-polls',
+    ],
+
+    dependency_links = [
+        "http://github.com/mvpdev/django-eav/tarball/master#egg=django-eav",
+        "http://github.com/daveycrockett/rapidsms-polls/tarball/master#egg=rapidsms-polls",
+    ],
+
+    description='The uReport social advocacy program deployed in Uganda.',
+    long_description=open('README.rst').read(),
+    author='David McCann',
+    author_email='david.a.mccann@gmail.com',
+
+    url='http://github.com/daveycrockett/rapidsms-ureport',
+    download_url='http://github.com/daveycrockett/rapidsms-ureport/downloads',
+
+    include_package_data=True,
+
+    packages=['ureport'],
+
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
