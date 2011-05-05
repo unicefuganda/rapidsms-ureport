fffc19055d65ef63abc599bd5511b0b02e624ef1
David McCann <david.a.mccann@gmail.com>
2011-05-05 12:27:31 +0300

    added district type poll

--- ureport/models.py
+++ ureport/models.py
@@ -1,10 +1,16 @@
 from django.db import models
-from poll.models import Poll
+from poll.models import Poll, LocationResponseForm, STARTSWITH_PATTERN_TEMPLATE
 from rapidsms.models import Contact
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 from django.contrib.sites.managers import CurrentSiteManager
 from django.conf import settings
+from simple_locations.models import Area, AreaType
+from eav.models import Attribute
+from django.core.exceptions import ValidationError
+
+import re
+import difflib
 
 class IgnoredTags(models.Model):
     poll = models.ForeignKey(Poll)
@@ -26,4 +32,26 @@ class MassText(models.Model):
             ("can_message", "Can send messages, create polls, etc"),
         )
 
+def parse_district_value(value):
+    location_template = STARTSWITH_PATTERN_TEMPLATE % '[a-zA-Z]*'
+    regex = re.compile(location_template)
+    try:
+        if regex.search(value):
+            spn = regex.search(value).span()
+            location_str = value[spn[0]:spn[1]]
+            area = None
+            area_names = Area.objects.filter(kind__name='district').values_list('name', flat=True)
+            area_names_lower = [ai.lower() for ai in area_names]
+            area_names_matches = difflib.get_close_matches(location_str.lower(), area_names_lower)
+            if area_names_matches:
+                area = Area.objects.get(name__iexact=area_names_matches[0], kind__name='district')
+                return area
+    except:
+        pass
+    raise ValidationError("We didn't recognize your district.  Please carefully type the name of your district and re-send.")
 
+Poll.register_poll_type('district', 'District Response', parse_district_value, db_type=Attribute.TYPE_OBJECT,\
+                        view_template='polls/response_location_view.html',
+                        edit_template='polls/response_location_edit.html',
+                        report_columns=(('Text','text'),('Location','location'),('Categories','categories')),
+                        edit_form=LocationResponseForm)
