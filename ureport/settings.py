49843efc7403c6b51d001fa244769cdb593a0b29
Assey Alfred <asseym@gmail.com>
2011-05-19 11:25:41 +0300

    added AUTHENTICATED_TABS list

--- ureport/settings.py
+++ ureport/settings.py
@@ -29,3 +29,9 @@ min_lat = '-1.0326589311777759'
 max_lat = '4.280680030820496'
 
 colors = ['#4572A7', '#AA4643', '#89A54E', '#80699B', '#3D96AE', '#DB843D', '#92A8CD', '#A47D7C', '#B5CA92']
+
+AUTHENTICATED_TABS = [
+    ("rapidsms.contrib.messagelog.views.message_log",       "Message Log"),
+    ("polls",                                               "Polls"),
+    ("poll_dashboard",                                    "Polls Dashboard"),
+]
