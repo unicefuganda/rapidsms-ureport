418030e40c62ec9573815fd27be38c587868d77b
David McCann <david.a.mccann@gmail.com>
2011-05-23 11:46:00 +0300

    s

--- ureport/settings.py
+++ ureport/settings.py
@@ -3,7 +3,7 @@
 ######################
 drop_words=['the','and','a','at','are','by','is','it','but','for','on','of','to','be','or','not',]
 
-tag_cloud_size=40
+tag_cloud_size=60
 
 ######################
 # Map Settings       #
