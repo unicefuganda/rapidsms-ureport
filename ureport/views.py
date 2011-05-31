17773cb426e06be6cdec7742d0cb91edd21dfb55
Mugisha Moses <moses@appfrica.org>
2011-05-31 16:08:59 +0300

    disable timezone for excel export

--- ureport/views.py
+++ ureport/views.py
@@ -23,6 +23,7 @@ from rapidsms_httprouter.models import Message, DIRECTION_CHOICES, STATUS_CHOICE
 from django.contrib.auth.decorators import login_required
 from django.contrib.contenttypes.models import ContentType
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.utils.datastructures import SortedDict
 
 from generic.views import generic, generic_dashboard
 
@@ -492,4 +493,37 @@ def poll_summary(request):
 def get_all_contacts(request):
     from uganda_common.utils import ExcelResponse
     contacts=Contact.objects.all()
-    return ExcelResponse(contacts)
+    export_data_list=[]
+    for contact in contacts:
+        if contact.name:
+            export_data=SortedDict()
+            export_data['name']=contact.name
+            if contact.gender:
+                export_data['sex']=contact.gender
+            else:
+                export_data['sex']='N/A'    
+            if contact.birthdate:
+                try:
+                    contact.birthdate.tzinfo=None
+                    export_data['age']=(datetime.datetime.now()-contact.birthdate).days/365
+                except:
+                    continue
+            else:
+                export_data['age']='N/A'
+            if contact.reporting_location:
+                export_data['district']=contact.reporting_location.name
+            else:
+                export_data['district']='N/A'
+            if contact.village:
+                export_data['village']=contact.village.name
+            else:
+                export_data['village']='N/A'
+            if contact.groups.count()>0:
+                export_data['group']=contact.groups.all()[0].name
+            else:
+                export_data['group']='N/A'
+
+            export_data_list.append(export_data)
+
+    response = ExcelResponse(export_data_list)
+    return response
