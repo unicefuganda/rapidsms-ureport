0578cc6c55859f00df84dbd401382af72dbb0866
Mugisha Moses <moses@appfrica.org>
2011-08-29 16:37:35 +0300

    ureporters export

--- ureport/settings.py
+++ ureport/settings.py
@@ -4,3 +4,6 @@
 drop_words=['the','and','a','at','are','by','is','it','but','for','on','of','to','be','or','not',]
 
 tag_cloud_size=60
+
+import os
+UREPORT_ROOT = os.path.dirname(os.path.realpath(__file__))
