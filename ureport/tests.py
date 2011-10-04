1f5ac411dc715c6b9c553732bce7aa07e74839f0
Mugisha Moses <mossplix@gmail.com>
2011-10-04 13:45:36 +0300

    youthpoll tests

--- ureport/tests.py
+++ ureport/tests.py
@@ -1,13 +1,18 @@
 """
 Basic tests for RapidSMS-Ureport
 """
+from django.contrib.auth.models import User, Group
 
 from django.test import TestCase
-from models import Flag, MessageFlag
+from models import Flag, MessageFlag, Script
 from rapidsms.models import Contact, Connection, Backend
 from rapidsms.messages.incoming import IncomingMessage
 from rapidsms_httprouter.models import Message
 from rapidsms_httprouter.router import get_router
+from django.core.management import call_command
+from rapidsms_xforms.models import XForm
+from poll.models import Poll
+from script.utils.handling import find_closest_match
 
 class ModelTest(TestCase): #pragma: no cover
 
@@ -18,32 +23,47 @@ class ModelTest(TestCase): #pragma: no cover
 
         self.backend = Backend.objects.create(name='test')
         self.connection = Connection.objects.create(identity='11235811', backend=self.backend)
-        #give the connection a contact
+        self.user,created=User.objects.get_or_create(username="admin")
+        self.router=get_router()
+        #create test contact
         self.connection.contact = Contact.objects.create(name='Anonymous User')
         self.connection.save()
+        #create message flags
         word_list=['zombies','inferi','waves','living dead','monsters']
         for word in word_list:
             flag=Flag.objects.create(name=word)
+        #create test group
+        self.gem_group=Group.objects.create(name="GEM")
 
-    def fakeIncoming(self, message, connection=None):
-        if connection is None:
-            connection = self.connection
-        router = get_router()
-        router.handle_incoming(connection.backend.name, connection.identity, message)
 
-    def spoof_incoming_obj(self, message, connection=None):
-        if connection is None:
-            connection = Connection.objects.all()[0]
-        incomingmessage = IncomingMessage(connection, message)
-        incomingmessage.db_message = Message.objects.create(direction='I', connection=Connection.objects.all()[0], text=message)
-        return incomingmessage
+    def fakeIncoming(self, message, connection):
+        self.router.handle_incoming(connection.backend.name, connection.identity, message)
 
+    def assertInteraction(self, connection, incoming_message, expected_response):
+        incoming_obj = self.router.handle_incoming(connection.backend.name, connection.identity, incoming_message)
+        self.assertEquals(Message.objects.filter(in_response_to=incoming_obj, text=expected_response).count(), 1)
 
     def testflaggedmessage(self):
-        connection = Connection.objects.all()[0]
         #create flagged messages
-
-        incomingmessage = self.fakeIncoming('My village is being invaded by an army of zombies')
+        #reload the connection object
+        connection=Connection.objects.all()[0]
+        incomingmessage = self.fakeIncoming('My village is being invaded by an army of zombies',self.connection)
         self.assertEquals(MessageFlag.objects.count(), 1)
+
+    def testyouthgrouppoll(self):
+        groups=["GEM","gem","GEM group","it is GEM"]
+        for g in groups:
+            group = find_closest_match(g, Group.objects)
+            self.assertEqual(self.gem_group,group)
+
+
+    def testPost_syncdb_handlers(self):
+         #call syncdb
+         call_command("syncdb")
+         self.assertEquals(Script.objects.count(),1)
+         self.assertEquals(Poll.objects.count(), 6)
+
+
+        
 
 
