4c85b7b21f8c39ef0ddce8dff5ce39626c26e2d8
Mugisha Moses <mossplix@gmail.com>
2011-10-12 15:52:26 +0300

    assign to group

--- ureport/management/commands/assign_to_group.py
+++ ureport/management/commands/assign_to_group.py
@@ -1,7 +1,11 @@
 from django.core.management.base import BaseCommand
+from django.db.utils import IntegrityError
 from script.models import ScriptSession,Script
 import traceback
-from django.contrib.auth.models import Group
+import re
+from django.contrib.auth.models import Group, User
+from script.utils.handling import find_closest_match
+
 class Command(BaseCommand):
 
     def handle(self, **options):
@@ -12,24 +16,43 @@ class Command(BaseCommand):
             bosco=Group.objects.get(name="BOSCO")
             msc=Group.objects.get(name="MSC")
             cou=Group.objects.get(name="Church of Uganda")
-            catholic_secritariat=Group.objects.get(name="Catholic secritariat")
+            catholic_secritariat=Group.objects.get(name="Catholic secretariat")
             groups={
                 catholic_secritariat:["catholic",'secritariat'],
                 world_vision:["world","vision"],
-                gem:["gem"],
+                gem:["gem","eduction movement"],
                 bosco:["bosco"],
                 msc:["muslim","supreme"],
                 cou:["church"],
             }
             for response in group_poll_responses:
                 txt=response.message.text.strip().lower()
-                for group, word_list in groups.items( ):
+
+                matched=False
+
+                for group, word_list in groups.items():
                     for word in word_list:
                         if word in txt.split():
-                            if group not in response.contact.groups.all():
+                            if response.contact and group not in response.contact.groups.all():
                                 response.contact.groups.add(group)
-                                print "added contact with response " +txt+ "to " +str(group.name)
+                                print "handled"
+                                print txt
+                                print response.contact.groups.all()
+                                matched=True
+                #get fuzzier
+                if not matched:
+                    for g in re.findall(r'\w+', txt):
+                        group = find_closest_match(g, Group.objects)
+                        if group and group in User.objects.get(username="js123powell").groups.all():
+                            if response.contact and  group not in response.contact.groups.all():
+                                response.contact.groups.add(group)
+                                print "handled"
+                                print txt
+                                print response.contact.groups.all()
+
+                        
 
 
+                            
         except Exception, exc:
             print traceback.format_exc(exc)
