391e480fd096e339418236cd50216ee3f39cc3b0
Mugisha Moses <mossplix@gmail.com>
2011-10-14 12:02:09 +0300

    some fixes

--- ureport/app.py
+++ ureport/app.py
@@ -20,11 +20,15 @@ class App (AppBase):
         w_regex=[]
         for word in flags:
             w_regex.append(one_template%str(word).strip())
-        reg =re.compile("|".join(w_regex))
+        reg =re.compile(r"|".join(w_regex))
         match= reg.search(message.text)
         if match:
             #we assume ureport is not the first sms app in the list so there is no need to create db_message
             if hasattr(message, 'db_message'):
                 db_message = message.db_message
-                MessageFlag.objects.create(message=db_message,flag=Flag.objects.get(name=match.groups()[1]))
+                try:
+                    flag=Flag.objects.get(name=[d for d in list(match.groups()) if d][1])
+                except Flag.DoesNotExist:
+                    flag=None
+                MessageFlag.objects.create(message=db_message,flag=flag)
         return False
---
+++ ureport/management/commands/migrate_flags.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+import re
+from ureport.models import Flag,MessageFlag
+
+class Command(BaseCommand):
+
+    def handle(self, **options):
+        fm=MessageFlag.objects.all()
+        for message in fm:
+            flags=Flag.objects.values_list('name',flat=True).distinct()
+            one_template=r"(.*\b(%s)\b.*)"
+            w_regex=[]
+            for word in flags:
+                w_regex.append(one_template%str(word).strip())
+            reg =re.compile("|".join(w_regex))
+            match= reg.search(message.message.text)
+            if match:
+                message.flag=Flag.objects.get(name=[d for d in list(match.groups()) if d][1])
+                message.save()
+
+        
+
+
