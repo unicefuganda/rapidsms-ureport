3a5bde295044a68bd16b9e15acc41e00568f429e
David McCann <david.a.mccann@gmail.com>
2011-10-21 12:20:50 +0300

    added partners

--- ureport/app.py
+++ ureport/app.py
@@ -2,7 +2,8 @@ import rapidsms
 import datetime
 
 from rapidsms.apps.base import AppBase
-from .models import Poll, Flag, MessageFlag
+from contact.models import Flag, MessageFlag
+from poll.models import Poll
 from django.db.models import Q
 from script.models import Script, ScriptProgress
 from rapidsms.models import Contact
@@ -12,23 +13,23 @@ class App (AppBase):
 
     def handle (self, message):
         if not message.connection.contact and not ScriptProgress.objects.filter(script__slug='ureport_autoreg', connection=message.connection).exists():
-            ScriptProgress.objects.create(script=Script.objects.get(slug="ureport_autoreg"),\
+            ScriptProgress.objects.create(script=Script.objects.get(slug="ureport_autoreg"), \
                                           connection=message.connection)
             return True
-        flags=Flag.objects.values_list('name',flat=True).distinct()
-        one_template=r"(.*\b(%s)\b.*)"
-        w_regex=[]
+        flags = Flag.objects.values_list('name', flat=True).distinct()
+        one_template = r"(.*\b(%s)\b.*)"
+        w_regex = []
         for word in flags:
-            w_regex.append(one_template%str(word).strip())
-        reg =re.compile(r"|".join(w_regex))
-        match= reg.search(message.text)
+            w_regex.append(one_template % str(word).strip())
+        reg = re.compile(r"|".join(w_regex))
+        match = reg.search(message.text)
         if match:
             #we assume ureport is not the first sms app in the list so there is no need to create db_message
             if hasattr(message, 'db_message'):
                 db_message = message.db_message
                 try:
-                    flag=Flag.objects.get(name=[d for d in list(match.groups()) if d][1])
+                    flag = Flag.objects.get(name=[d for d in list(match.groups()) if d][1])
                 except Flag.DoesNotExist:
-                    flag=None
-                MessageFlag.objects.create(message=db_message,flag=flag)
+                    flag = None
+                MessageFlag.objects.create(message=db_message, flag=flag)
         return False
