240b9562fc36c2ecb85b5f06eff7ae6878ee716a
David McCann <david.a.mccann@gmail.com>
2011-10-24 10:48:14 +0300

    fixed ureporter dump

--- ureport/management/commands/clean_ureporters.py
+++ ureport/management/commands/clean_ureporters.py
@@ -1,34 +1,76 @@
-import datetime
-
 from django.core.management.base import BaseCommand
-from script.models import *
-
-from rapidsms.models import Backend, Connection, Contact
-from rapidsms_httprouter.models import Message
-from django.utils.encoding import smart_unicode, smart_str
+import traceback
+import os
+from script.models import ScriptSession
+from ureport.settings import UREPORT_ROOT
+from rapidsms.models import Contact
+from django.utils.datastructures import SortedDict
+from poll.models import Poll
+import datetime
+import optparse
 
 class Command(BaseCommand):
 
-    help = """dumps all ureporters and their message histories
-    """
-
     def handle(self, **options):
-        print smart_str("ID\tname\tgender\tage\tgroups\tdistrict ID\tdistrict name\tvillage ID\tvillage name\tMESSAGE HISTORY")
-        for c in Contact.objects.all():
-            name = smart_unicode(c.name.replace("\n","").replace("\r","")) if c.name else u''
-            gender = c.gender or ''
-            age = str(int((datetime.datetime.now() - c.birthdate).days / 365.0)) if c.birthdate else ''
-            groups = ','.join(c.groups.values_list('name',flat=True))
-            msgs = []
-            for m in Message.objects.filter(connection__contact=c).order_by('date'):
-                msgs.append(smart_str(m.text.replace("\n","").replace("\r","")))
-                msgs.append("%s" % m.date)
-            message_history = smart_str("\t").join(msgs)
-            location_pk = str(c.reporting_location.pk) if c.reporting_location else ''
-            location_name = c.reporting_location.name if c.reporting_location else ''
-            village_pk = str(c.village.pk) if c.village else ''
-            village_name = c.village.name if c.village else ''
-            try:
-                print smart_str(smart_str("%d\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s") % (c.pk,name,gender,age,groups,location_pk,location_name,village_pk,village_name,message_history))
-            except UnicodeDecodeError:
-                continue
+        try:
+
+            from uganda_common.utils import ExcelResponse
+
+            excel_file_path = os.path.join(os.path.join(os.path.join(UREPORT_ROOT, 'static'), 'spreadsheets'), 'ureporters_full.xls')
+            contacts = Contact.objects.all()
+            export_data_list = []
+            for contact in contacts:
+                if contact.name:
+                    print "adding " + contact.name
+                    export_data = SortedDict()
+                    export_data['name'] = contact.name
+                    if contact.default_connection:
+                        export_data['mobile'] = contact.default_connection.identity
+                    else:
+                        export_data['mobile'] = "N/A"
+
+                    if contact.gender:
+                        export_data['sex'] = contact.gender
+                    else:
+                        export_data['sex'] = 'N/A'
+                    if contact.birthdate:
+
+                        #contact.birthdate.tzinfo = None
+                        #import pdb;pdb.set_trace()
+
+                        export_data['age'] = (datetime.datetime.now() - contact.birthdate).days / 365
+
+                    else:
+                        export_data['age'] = 'N/A'
+                    if contact.reporting_location:
+                        export_data['district'] = contact.reporting_location.name
+                    else:
+                        export_data['district'] = 'N/A'
+                    if contact.village:
+                        export_data['village'] = contact.village.name
+                    else:
+                        export_data['village'] = 'N/A'
+                    if contact.groups.count() > 0:
+                        export_data['group'] = contact.groups.all()[0].name
+                    else:
+                        export_data['group'] = 'N/A'
+                    if ScriptSession.objects.filter(connection__contact=contact).exists():
+                         export_data["join date"] = ScriptSession.objects.filter(connection__contact=contact)[0].start_time
+
+                    elif contact.default_connection and contact.default_connection.messages.exists():
+                        export_data["join date"] = contact.default_connection.messages.order_by('date')[0].date
+                    else:
+                        export_data["join date"] = "N/A"
+
+                    export_data["Total Poll Responses"] = contact.responses.count()
+                    mnum = 0
+                    for m in Message.objects.filter(connection__contact=c).order_by('date'):
+                        export_data["message %d" % mnum] = m.text
+                        export_data["message %d date" % mnum] = str(m.date)
+                        mnum += 1
+                    export_data_list.append(export_data)
+
+            ExcelResponse(export_data_list, output_name=excel_file_path, write_to_file=True)
+
+        except Exception, exc:
+            print traceback.format_exc(exc)
