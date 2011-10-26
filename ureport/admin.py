b6243cc5547e417439d8c2fc97f777587ee0538a
Mugisha Moses <mossplix@gmail.com>
2011-10-26 12:21:02 +0300

    admin for Quotebox

--- ureport/admin.py
+++ ureport/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from ureport.models import IgnoredTags
+from ureport.models import IgnoredTags,QuoteBox
 admin.site.register(IgnoredTags)
+admin.site.register(QuoteBox)
 
