a0c53fd5f9b76e74961baadb9dbb326a62abe4aa
David McCann <david.a.mccann@gmail.com>
2011-10-27 09:09:36 +0300

    removed district poll type logic

--- ureport/models.py
+++ ureport/models.py
@@ -41,22 +41,6 @@ class QuoteBox(models.Model):
         get_latest_by = 'creation_date'
 
 
-def parse_district_value(value):
-    location_template = STARTSWITH_PATTERN_TEMPLATE % '[a-zA-Z]*'
-    regex = re.compile(location_template)
-    toret = find_closest_match(value, Location.objects.filter(type__name='district'))
-    if not toret:
-        raise ValidationError("We didn't recognize your district.  Please carefully type the name of your district and re-send.")
-    else:
-        return toret
-
-Poll.register_poll_type('district', 'District Response', parse_district_value, db_type=Attribute.TYPE_OBJECT, \
-                        view_template='polls/response_location_view.html',
-                        edit_template='polls/response_location_edit.html',
-                        report_columns=(('Text', 'text'), ('Location', 'location'), ('Categories', 'categories')),
-                        edit_form=LocationResponseForm)
-
-
 def autoreg(**kwargs):
     connection = kwargs['connection']
     progress = kwargs['sender']
