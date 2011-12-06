a2a10e6a751ab5c2627892ed3f591a62041b761e
Mugisha Moses <mossplix@gmail.com>
2011-12-06 13:06:32 +0300

    fix how did u hear

--- ureport/views.py
+++ ureport/views.py
@@ -727,7 +727,7 @@ def ureporter_profile(request, connection_pk):
     gr_poll=Poll.objects.get(pk=121)
     if session.exists():
         try:
-            how_did_u_hear= session[0].responses.filter(response__poll=gr_poll, response__has_errors=False)
+            how_did_u_hear= session[0].responses.filter(response__poll=gr_poll)
         except ScriptResponse.DoesNotExist:
             how_did_u_hear="N/A"
 
