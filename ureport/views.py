e51d6a011079d9fa30892be9f47b83329f42cef8
Mugisha Moses <mossplix@gmail.com>
2012-01-24 17:37:02 +0300

    fix cloud

--- ureport/views.py
+++ ureport/views.py
@@ -165,7 +165,7 @@ def _get_tags(polls):
     cursor.execute(sql)
     rows=cursor.fetchall()
     rows_dict=dict(rows)
-    bl=list(IgnoredTags.objects.filter(pk__in=poll_pks).values_list("name",flat=True))
+    bl=list(IgnoredTags.objects.filter(poll__in=polls).values_list("name",flat=True))
     for key in rows_dict.keys():
         if len(key) > 2 and not key in drop_words+bl:
             word_count[str(key)]=int(rows_dict[key])
