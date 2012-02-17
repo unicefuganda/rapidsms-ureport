8d8aa94803a011b65b3b14dee879021423c03ef6
Mugisha Moses <mossplix@gmail.com>
2012-02-17 14:02:43 +0300

    fix disticts

--- ureport/views.py
+++ ureport/views.py
@@ -994,7 +994,6 @@ def new_poll(req):
             if hasattr(Contact, 'groups'):
                 groups = form.cleaned_data['groups']
 
-            districts = form.cleaned_data['districts']
             if len(districts):
                 contacts = Contact.objects.filter(Q(reporting_location__in=districts) | Q(groups__in=groups)).distinct()
             else:
@@ -1044,3 +1043,30 @@ def new_poll(req):
 
     return render_to_response('ureport/new_poll.html', {'form': form},
                               context_instance=RequestContext(req))
+
+from ussd.forms import YoForm
+from django.forms import ValidationError
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+import urllib
+
+
+def ussd(req, input_form=YoForm, request_method='POST', output_template='ussd/yo.txt'):
+    form = None
+    if request_method == 'GET' and req.GET:
+        form = input_form(req.GET)
+    elif request_method == 'POST' and req.POST:
+        form = input_form(req.POST)
+    if form and form.is_valid():
+        session = form.cleaned_data['transactionId']
+        request_string = form.cleaned_data['ussdRequestString']
+
+        response_screen = session.advance_progress(request_string)
+        action = 'end' if response_screen.is_terminal() else 'request'
+        return render_to_response(output_template, {
+            'response_content':urllib.quote(str(response_screen)),
+            'action':action,
+        }, context_instance=RequestContext(req))
+
+    return HttpResponse(status=404)
