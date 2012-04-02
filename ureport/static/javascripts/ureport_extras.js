2db4188c14b529ad35d1b07243dc3832a7a53613
Mugisha Moses <mossplix@gmail.com>
2012-04-02 15:24:37 +0300

    some cleanup

--- ureport/static/javascripts/ureport_extras.js
+++ ureport/static/javascripts/ureport_extras.js
@@ -1,22 +1,4 @@
-jQuery.fn.forms = function () {
-  var btn = $("input[type=submit]");
-  btn.parent().prepend("<span class=\"loader\" title=\"Sending...\">&nbsp;</span>");
 
-  this.bind("submit",
-            function() {
-              $("input[type=submit]").hide();
-              $("span.cancel").hide();
-              $("span.loader").show();
-            }
-           );
-};
-
-jQuery.fn.addHover = function() {
-  return this.hover(
-    function(){ jQuery(this).addClass("hover"); },
-    function(){ jQuery(this).removeClass("hover"); }
-  )
-};
 
 (function($) {
     $.PeriodicalUpdater = function(options, callback){
@@ -72,3 +54,5 @@ jQuery.fn.addHover = function() {
         }
     };
 })(jQuery);
+
+
