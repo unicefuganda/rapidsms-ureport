e6f5830346656da4e20f918fc81880cfa77181ed
Mugisha Moses <mossplix@gmail.com>
2012-05-30 14:51:29 +0300

    fix transaction error

--- ureport/tasks.py
+++ ureport/tasks.py
@@ -5,6 +5,7 @@ from rapidsms_httprouter.models import Message
 from uganda_common.utils import ExcelResponse
 from ureport.models import *
 from poll.models import ResponseCategory, Poll, Response
+from django.db import transaction
 
 
 
@@ -19,7 +20,7 @@ def reprocess_responses(poll):
     poll.reprocess_responses()
 
 @task
-def process_message(message):
+def process_message(message,**kwargs):
     if hasattr(message, 'db_message'):
         message = message.db_message
     try:
@@ -28,8 +29,10 @@ def process_message(message):
         if alert_setting.value=="true":
             alert,_=MessageAttribute.objects.get_or_create(name="alert")
             msg_a=MessageDetail.objects.create(message=message,attribute=alert,value='true')
-    except Settings.DoesNotExist:
-        pass
+    except IntegrityError,e:
+        transaction.rollback()
+        process_message.retry(args=[message], exc=e, countdown=30, kwargs=kwargs)
+
 
 
 
