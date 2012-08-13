d739e8bed18ae67dd28532d8b35e70cd48fcf7ac
Mugisha Moses <mossplix@gmail.com>
2012-08-13 17:30:05 +0300

    merge fix

--- ureport/management/commands/fix_kannel.py
+++ ureport/management/commands/fix_kannel.py
@@ -13,16 +13,16 @@ from rapidsms_httprouter.models import Message
 from django.db import connection
 from optparse import OptionParser, make_option
 import re
-
+from rapidsms.models import Connection
+from rapidsms_httprouter.router import get_router
 
 class Command(BaseCommand):
 
 
     def handle(self, **options):
 
-        poll =Poll.objects.get(pk=236)
         #file1=open("/home/mossplix/log_8.txt")
-        file2=open("/home/mossplix/incoming.log")
+        file2=open("/home/mossplix/incoming2.log")
         files=[file2]
         num=re.compile('([0-9]+)')
         for f in files:
@@ -30,18 +30,26 @@ class Command(BaseCommand):
             for line in lines:
                 parts=line.strip().rsplit('[FID:]')[1].split('] [')
                 identity=num.search(parts[1]).groups()[0]
-                message_text=parts[4].rsplit(':')[-1]
+                message_text=parts[4].split(':')[2]
+
                 try:
                     connection=Connection.objects.get(identity=identity)
-                    msg=Message.objects.filter(connection__identity=identity,text=message_text,direction="I")
+                    msg=Message.objects.filter(connection__identity=identity,text__icontains=message_text,direction="I")
                     if msg.exists():
                         pass
                     else:
+                        router=get_router()
+                        router.handle_incoming(connection.backend.name, connection.identity, message_text)
 
-                        msg=Message.objects.create(connection__identity=identity,text=message_text,direction="I")
-                        print "created "+msg.text
-                        if poll.objects.filter(pk=connection.contact.pk):
-                            poll.process_response(msg)
+#                        with open("/home/mossplix/desc.log", "r+") as f:
+#                            old = f.read() # read everything in the file
+#                            f.seek(0) # rewind
+#                            f.write(line + old)
+
+#                        msg=Message.objects.create(connection__identity=identity,text=message_text,direction="I")
+#                        print "created "+msg.text
+#                        if poll.objects.filter(pk=connection.contact.pk):
+#                            poll.process_response(msg)
                 except Connection.DoesNotExist:
                     pass
 
