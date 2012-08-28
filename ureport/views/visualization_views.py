f7233a784610f5e806b68ec99f548d0a4ea7333f
Mugisha Moses <mossplix@gmail.com>
2012-08-29 00:37:25 +0300

    fix tags

--- ureport/views/visualization_views.py
+++ ureport/views/visualization_views.py
@@ -40,8 +40,7 @@ def best_visualization(request, poll_id=None):
         'module': module,
         'rate': int(rate),
         }
-    if poll.type == Poll.TYPE_TEXT \
-        and ResponseCategory.objects.filter(response__poll=poll).exists():
+    if poll.type == Poll.TYPE_TEXT and not  ResponseCategory.objects.filter(response__poll=poll).exists():
         dict.update({'tags': _get_tags(polls),
                     'responses': _get_responses(poll),
                     'poll_id': poll.pk})
