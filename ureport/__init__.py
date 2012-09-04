a1eb5eebb876ba7ffdd401f6d2d79ac49d4354f2
Mugisha Moses <mossplix@gmail.com>
2012-09-04 12:06:10 +0300

    fix settings bs

--- ureport/__init__.py
+++ ureport/__init__.py
@@ -2,18 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from django.template import loader
-try:
-    from django.conf.settings import DEBUG
-except AttributeError:
-    DEBUG =False
-
 
 template_cache = {}
 original_get_template = loader.get_template
 def cached_get_template(template_name):
     global template_cache
     t = template_cache.get(template_name,None)
-    if not t or DEBUG:
+    if not t :
         template_cache[template_name] = t = original_get_template(template_name)
     return t
 loader.get_template = cached_get_template
