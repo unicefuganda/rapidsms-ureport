9e9a0b30d5beca69277f55cde8f4242fdd3c04ad
Mugisha Moses <mossplix@gmail.com>
2012-09-14 12:19:37 +0300

    fix sorters

--- ureport/views/utils/sorters.py
+++ ureport/views/utils/sorters.py
@@ -1,4 +1,4 @@
 class ContactsViewSorter(object):
     def sort(self, column, object_list, ascending=True):
-        query = "select id,%s from contacts_export order by %s" % ('asc' if ascending else 'desc', column,column)
+        query = "select id,%s from contacts_export order by %s %s" % ( column,column,'asc' if ascending else 'desc')
         return object_list.raw(query)
