6242dd65fee76699e0df3e9bf959847e195277c5
wanderanimrod <wanderanimrod@gmail.com>
2013-04-16 14:22:45 +0300

    [nim] Renaming functional tests to start with funct_

---
+++ ureport/tests/functional/funct_poll_view.py
@@ -0,0 +1,21 @@
+from splinter import Browser
+from rapidsms.models import Contact
+from rapidsms.models import Connection
+from ureport.tests.functional.splinter_wrapper import SplinterTestCase
+
+BROWSER = Browser('firefox')
+
+
+class PollViewTest(SplinterTestCase):
+
+    #fixtures = ['0004_migration_initial_data.json']
+
+    def setUp(self):
+        self.browser = BROWSER
+        self.open('/')
+
+    def tearDown(self):
+        pass
+
+    def test_something_simple(self):
+        self.assertTrue(True)
