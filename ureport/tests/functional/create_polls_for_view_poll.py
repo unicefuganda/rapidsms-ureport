faf3160a39ec6f743d3a4ab4d1d5aed9f2f4dd15
wanderanimrod <wanderanimrod@gmail.com>
2013-04-18 15:39:40 +0300

    [dj, nim] Faking incoming messages for the created polls.

--- ureport/tests/functional/create_polls_for_view_poll.py
+++ ureport/tests/functional/create_polls_for_view_poll.py
@@ -2,10 +2,14 @@ from datetime import datetime
 from django.contrib.auth.models import Group, User
 from rapidsms.models import Connection, Contact, Backend
 from poll.models import Poll
+from rapidsms_httprouter.models import Message
+from rapidsms_httprouter.router import get_router
+
 
 def create_group(group_name):
     group = Group.objects.create(name=group_name)
     return group
+
 
 def create_user(username, email, group):
     user1 = User.objects.create(username=username, email=email)
@@ -13,10 +17,12 @@ def create_user(username, email, group):
     user1.save()
     return user1
 
+
 def create_connection(identity, contact, backend):
-    conn1 = Connection.objects.create(identity=identity, backend=backend)
-    conn1.contact = contact
-    conn1.save()
+    connection = Connection.objects.create(identity=identity, backend=backend)
+    connection.contact = contact
+    connection.save()
+    return connection
 
 def create_poll(contacts, user):
     poll_name = "functional_test"
@@ -28,22 +34,35 @@ def create_poll(contacts, user):
     return poll
 
 
+def fake_incoming(connection, incoming_message):
+    router = get_router()
+    return router.handle_incoming(connection.backend.name, connection.identity, incoming_message)
+
+
 def create_eleven_polls_for_view_polls():
     group = create_group(group_name='Pagination Group')
     user1 = create_user(username="foo", email='foo@bar.com', group=group)
     user2 = create_user(username='fred', email='shaggy@scooby.com', group=group)
 
-    contact1 = Contact.objects.create(pk=567, name='FT1', user=user1, gender='M',birthdate=datetime.now(),language="en")
-    contact2 = Contact.objects.create(pk=765, name='FT2', user=user2, gender='M',birthdate=datetime.now(),language="en")
+    contact1 = Contact.objects.create(pk=567, name='FT1', user=user1, gender='M', birthdate=datetime.now(),
+                                      language="en")
+    contact2 = Contact.objects.create(pk=765, name='FT2', user=user2, gender='M', birthdate=datetime.now(),
+                                      language="en")
     contacts = [contact1,contact2]
 
-    backend = Backend.objects.create(name='test_backend')
-    create_connection(identity='0794339344',contact=contact1,backend=backend)
-    create_connection(identity='0794339427',contact=contact2,backend=backend)
+    backend = Backend.objects.create(name='dmark')
+
+    connection1 = create_connection('0794339344', contact1, backend)
+    connection2 = create_connection('0794339427', contact2, backend)
+    connections = [connection1, connection2]
 
     polls = []
     for poll in range(0, 10):
         poll = create_poll(contacts, user1)
+        poll.add_yesno_categories()
         polls.append(poll)
+
+        for connection in connections:
+            fake_incoming(connection, 'yes')
 
     return polls
