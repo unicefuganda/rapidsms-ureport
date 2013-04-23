de8a8c9914d3feb736f25e7401ed03267f2eb047
Jim Barritt <jim@planet-ix.com>
2013-04-23 11:09:54 +0300

    [jmdb] Adding an access log as middleware

---
+++ ureport/middleware/access_log.py
@@ -0,0 +1,24 @@
+import logging
+log = logging.getLogger(__name__)
+
+
+
+class UreportAccessLogMiddleware:
+    # THis doesnt ever seem to get called!!
+    def process_exception(self, request, exception):
+        import traceback
+        import sys
+        exc_info = sys.exc_info()
+        log.exception(exception)
+        log.info('\n'.join(traceback.format_exception(*(exc_info or sys.exc_info()))))
+
+
+    def process_request(self, request):
+        log.info(request.method + " \t: " + request.path)
+
+
+
+# Can't get this to work, it seems to mess with the response.
+#    def process_response(self, request, response):
+#        log.info(request.path + " : returned " + str(response.status_code))
+
