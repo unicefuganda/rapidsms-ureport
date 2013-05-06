272fc765f74ecf75a50f34ed8dd1a47464f1e6b8
wanderanimrod <wanderanimrod@gmail.com>
2013-05-06 12:46:50 +0300

    [nim] Refactoring the test to a cleaner state

--- ureport/tests/unit/context_processor_test.py
+++ ureport/tests/unit/context_processor_test.py
@@ -1,20 +1,28 @@
 from unittest import TestCase
-from fixtures.create_polls_for_view_poll import create_and_start_polls
 from ureport_project import settings
 from ureport_project.rapidsms_ureport.ureport.context_processors import has_valid_pagination_limit
 
 
 class ContextProcessorTest(TestCase):
 
-    def test_should_fail_if_pagination_limit_does_not_exist_in_settings(self):
-        modified_settings = settings.__dict__.pop('PAGINATION_LIMIT')
-        self.assertFalse(has_valid_pagination_limit(modified_settings))
+    def tearDown(self):
+        self.clear_settings()
+
+    def test_should_not_have_valid_pagination_limit_if_settings_are_not_present(self):
+        self.clear_settings()
+        self.assertFalse(has_valid_pagination_limit(settings))
 
     def test_should_fail_if_pagination_limit_is_no_a_number(self):
-        bad_settings = settings.__dict__['PAGINATION_LIMIT'] = 'text'
-        self.assertFalse(has_valid_pagination_limit(bad_settings))
+        settings.PAGINATION_LIMIT = 'text'
+        self.assertFalse(has_valid_pagination_limit(settings))
 
     def test_should_pass_if_pagination_limit_in_settings_is_a_number(self):
         settings.PAGINATION_LIMIT = 10
-        good_settings = settings
-        self.assertTrue(has_valid_pagination_limit(good_settings))
+        self.assertTrue(has_valid_pagination_limit(settings))
+
+    def clear_settings(self):
+        try:
+            return delattr(settings, 'PAGINATION_LIMIT')
+        except AttributeError:
+            pass
+
