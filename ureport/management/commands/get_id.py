816adc97bc1002d094595b60a5b78fd28c87c829
Kenneth Matovu <matovken@gmail.com>
2013-05-08 08:37:00 +0300

    adding national pulse

---
+++ ureport/management/commands/get_id.py
@@ -0,0 +1,19 @@
+import json
+from django.core.management import BaseCommand
+from django.db.models.aggregates import Count
+from message_classifier.models import IbmCategory
+from rapidsms.contrib.locations.models import Location
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        l = Location.objects.filter(type='district').distinct()
+
+        s = IbmCategory.objects.filter(ibmmsgcategory__score__gte=0.4,
+                                       ibmmsgcategory__msg__connection__contact__reporting_location__in=l).annotate(
+            total=Count('ibmmsgcategory')).values('total', 'name',
+                                                  'ibmmsgcategory__msg__connection__contact__reporting_location__name').\
+            exclude(name__in=['family & relationships', "energy", "u-report", "social policy", "employment"])
+
+        data = json.dumps(list(s))
+        print data
