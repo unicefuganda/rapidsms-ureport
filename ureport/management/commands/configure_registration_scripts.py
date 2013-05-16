a3a394eb35658df2d190adec78151c923a53c34b
arghac <007.argha@gmail.com>
2013-05-16 16:00:46 +0300

    [Argha][dj] reading registration messages from a file

--- ureport/management/commands/configure_registration_scripts.py
+++ ureport/management/commands/configure_registration_scripts.py
@@ -5,6 +5,16 @@ __author__ = 'argha'
 from django.core.management.base import BaseCommand
 
 
+def parse_line(line,separator='='):
+    tokens_list = line.split(separator)
+    return  int(tokens_list[0]), tokens_list[1].strip('\n')
+
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        configure_messages_for_script('ureport_autoreg2',{0:'bar0',1:'bar1', 2:'bar2', 3:'bar3'})
+        file_as_list = list(open("messages.txt"))
+        messages_dict = {}
+        for line in file_as_list:
+            key,value = parse_line(line)
+            messages_dict[key] = value
+        configure_messages_for_script('ureport_autoreg2', messages_dict)
