b755ef76d2f773ad06ee88491535ca5e8d1c8aad
arghac <007.argha@gmail.com>
2013-07-09 12:56:00 +0300

    [Argha] accept file with custom messages as an argument to management command.

--- ureport/management/commands/configure_registration_scripts.py
+++ ureport/management/commands/configure_registration_scripts.py
@@ -12,9 +12,13 @@ def parse_line(line,separator='='):
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        file_as_list = list(open("messages.txt"))
-        messages_dict = {}
-        for line in file_as_list:
-            key,value = parse_line(line)
-            messages_dict[key] = value
-        configure_messages_for_script('ureport_autoreg2', messages_dict)
+        if len(args) < 1:
+            print("You must pass the file with custom messages as an argument.")
+            exit(-1)
+        else:
+            file_as_list = list(open(args[0]))
+            messages_dict = {}
+            for line in file_as_list:
+                key,value = parse_line(line)
+                messages_dict[key] = value
+            configure_messages_for_script('ureport_autoreg2', messages_dict)
