b313d962d88be552c01808023d2964cefa499b8f
Felix Kitaka <webash.uganda@gmail.com>
2013-08-05 15:59:48 +0300

    [Felix, nim] Debug tags to see why dumping functional test failure screenshots to go target directory fails on ci and not on the local box

--- ureport/tests/functional/take_screenshot.py
+++ ureport/tests/functional/take_screenshot.py
@@ -3,6 +3,7 @@ from functools import wraps
 
 
 def take_screenshot_on_failure(test):
+    print("************************************\n\n TAKE SCREENSHOT DECORATOR CALLED ******************************\n\n")
     @wraps(test)
     def inner_decorator(*args, **kwargs):
         try:
@@ -10,10 +11,11 @@ def take_screenshot_on_failure(test):
         except Exception, e:
             try:
                 test_object = args[0]
-
+                print("************************************\n\n EXCEPTION CAUGHT ******************************\n\n")
                 timestamp = datetime.datetime.now().isoformat().replace(':', '')
                 filename = r'target/reports/functional-test/screenshots/failure_%s.png' % timestamp
                 test_object.browser.driver.save_screenshot(filename)
+                print("************************************\n\n SCREENSHOT TAKEN ******************************\n\n")
                 print r'Dumped screen shot of failure to [%s]' % (filename,)
             except Exception, ex:
                 print ex.message
