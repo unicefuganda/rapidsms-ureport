a9c1be95237bad4566f679fb76b8016e1fe7c805
wanderanimrod <wanderanimrod@gmail.com>
2013-08-05 16:36:13 +0300

    [nim, mj] Removing debug statements from screenshot decorator

--- ureport/tests/functional/take_screenshot.py
+++ ureport/tests/functional/take_screenshot.py
@@ -3,7 +3,6 @@ from functools import wraps
 
 
 def take_screenshot_on_failure(test):
-    print("************************************\n\n TAKE SCREENSHOT DECORATOR CALLED ******************************\n\n")
     @wraps(test)
     def inner_decorator(*args, **kwargs):
         try:
@@ -11,11 +10,9 @@ def take_screenshot_on_failure(test):
         except Exception, e:
             try:
                 test_object = args[0]
-                print("************************************\n\n EXCEPTION CAUGHT ******************************\n\n")
                 timestamp = datetime.datetime.now().isoformat().replace(':', '')
                 filename = r'target/reports/functional-test/screenshots/failure_%s.png' % timestamp
                 test_object.browser.driver.save_screenshot(filename)
-                print("************************************\n\n SCREENSHOT TAKEN ******************************\n\n")
                 print r'Dumped screen shot of failure to [%s]' % (filename,)
             except Exception, ex:
                 print ex.message
