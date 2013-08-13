bb27ee943a2b65150b76caaee1b9e14ebfc829d2
Luiza Nunes <lnunes@Luizas-MacBook-Pro.local>
2013-08-13 10:22:56 +0300

    [Argha,Luiza] refactoring test structure. WIP

--- ureport/tests/functional/create_poll_utils.py
+++ ureport/tests/functional/create_poll_utils.py
@@ -1,6 +1,6 @@
 from splinter import Browser
 from django.contrib.auth.models import Group, User
-from rapidsms.models import Connection
+from rapidsms.models import Connection, Contact
 from poll.models import Poll
 from rapidsms_httprouter.models import Message
 from rapidsms_httprouter.router import get_router
@@ -17,6 +17,10 @@ def create_user(username, email, group):
     user1.save()
     return user1
 
+def create_contact(name, user, gender, birthdate, language):
+    contact = Contact.objects.create(name=name, user=user, gender=gender, birthdate=birthdate, language=language)
+    return contact
+
 
 def create_connection(identity, contact, backend):
     connection = Connection.objects.create(identity=identity, backend=backend)
@@ -30,7 +34,6 @@ def create_poll(user):
     question = "from FT with love!"
     poll = Poll.objects.create(name=poll_name, question=question, user=user, type=Poll.TYPE_TEXT)
     return poll
-
 
 def add_contacts_to_poll(poll, contacts):
     for contact in contacts:
--- ureport/tests/functional/funct_start_poll.py
+++ ureport/tests/functional/funct_start_poll.py
@@ -1,6 +1,5 @@
-import time
-
 from poll.models import Poll
+from poll.tests.TestScript import fake_incoming
 from ureport.tests.functional.splinter_wrapper import SplinterTestCase
 from ureport.tests.functional.take_screenshot import take_screenshot_on_failure
 from ureport_project.rapidsms_ureport.ureport.tests.functional.create_poll_for_tests import start_poll_queues_messages_in_table
@@ -13,18 +12,26 @@ class UreportTest(SplinterTestCase):
     def setUp(self):
         self.browser = get_browser()
         self.open('/')
+        self.poll_id, self.contacts_count = start_poll_queues_messages_in_table()
 
     def tearDown(self):
         self.open('/account/logout')
         self.browser.quit()
 
+    def test_that_poll_status_changes_when_started(self):
+        self.create_and_sign_in_admin("argha", "a")
+        self.open("/view_poll/%s" % self.poll_id)
+        self.browser.find_link_by_text('Start Poll').first.click()
+        assert self.browser.is_text_present('Close Poll')
+        newly_added_poll = Poll.objects.get(id=self.poll_id)
+        self.assertThat(newly_added_poll.start_date is not None)
+
     @take_screenshot_on_failure
     def test_should_match_poll_question_to_message_text(self):
-        self.poll_id, self.contacts_count = start_poll_queues_messages_in_table(self)
-
         newly_added_poll = Poll.objects.get(id=self.poll_id)
 
-        time.sleep(5)
+        self.test_that_poll_status_changes_when_started()
+
         self.assertEquals(newly_added_poll.messages.count(), 2)
         self.assertEquals(newly_added_poll.messages.all()[0].text, newly_added_poll.question)
         self.assertEquals(newly_added_poll.messages.all()[1].text, newly_added_poll.question)
@@ -32,5 +39,10 @@ class UreportTest(SplinterTestCase):
         self.assertEquals(newly_added_poll.messages.filter(status='Q').count(), 2)
         self.assertEquals(newly_added_poll.messages.filter(status='Q')[0].text, newly_added_poll.question)
         self.assertEquals(newly_added_poll.messages.filter(status='Q')[1].text, newly_added_poll.question)
+    #
+    # def test_ureporter_can_respond_to_poll_question(self):
+    #     fake_incoming()
+
+
 
 
