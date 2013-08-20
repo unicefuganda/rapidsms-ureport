3cdf559802420c4d2b4646406cc3c0ea8b087b0e
Kenneth Matovu <matovken@gmail.com>
2013-08-20 09:17:27 +0300

    exporting all ureporters

--- ureport/management/commands/get_id.py
+++ ureport/management/commands/get_id.py
@@ -12,8 +12,12 @@ class Command(BaseCommand):
             print "On Poll ", poll.pk, "===>", poll.identity
             poll_export = SortedDict()
             poll_export['Identity'] = poll.pk
-            poll_export['District'] = poll.contact.reporting_location
             poll_export['Phone'] = poll.identity
-            poll_export['Gender'] = poll.contact.gender
+            if poll.contact:
+                poll_export['District'] = poll.contact.reporting_location
+                poll_export['Gender'] = poll.contact.gender
+            else:
+                poll_export['District'] = "Not known"
+                poll_export['Gender'] = "Not known"
             poll_list.append(poll_export)
         ExcelResponse(poll_list, output_name='/home/kenneth/all_contacts.xlsx', write_to_file=True)
