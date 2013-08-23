90c78f47f0bee525de7f20cf88b274c58d7b8cf2
Luiza Nunes <luhhsnunes@gmail.com>
2013-08-23 14:41:43 +0300

    [Luiza, Felix] Added a step in order to verify if the text is present

--- ureport/tests/functional/poll_base.py
+++ ureport/tests/functional/poll_base.py
@@ -6,7 +6,11 @@ class PollBase(PollAssertions):
     def start_poll(self):
         poll_url = "/view_poll/%s" % self.poll.id
         self.create_and_sign_in_admin("argha", "a", poll_url)
+
+        assert self.browser.is_text_present('Start Poll',5)
+
         self.browser.find_link_by_text('Start Poll').first.click()
+
         assert self.browser.is_text_present('Close Poll')
 
     def get_poll(self, poll_id):
