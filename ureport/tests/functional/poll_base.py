2509dd12588fc535efa27a3bafa60951282ffb13
Luiza Nunes <luhhsnunes@gmail.com>
2013-08-23 16:22:09 +0300

    [Luiza, Felix] Adding the annotation to take screenshot when failure

--- ureport/tests/functional/poll_base.py
+++ ureport/tests/functional/poll_base.py
@@ -1,13 +1,16 @@
 from poll.models import Poll
 from ureport.tests.functional.poll_assertions import PollAssertions
+from ureport.tests.functional.take_screenshot import take_screenshot_on_failure
 
 
 class PollBase(PollAssertions):
+
+    @take_screenshot_on_failure
     def start_poll(self):
         poll_url = "/view_poll/%s" % self.poll.id
         self.create_and_sign_in_admin("argha", "a", poll_url)
 
-        assert self.browser.is_text_present('Start Poll',5)
+        assert self.browser.is_text_present('Start Poll',10)
         self.browser.find_link_by_text('Start Poll').first.click()
 
         assert self.browser.is_text_present('Close Poll')
