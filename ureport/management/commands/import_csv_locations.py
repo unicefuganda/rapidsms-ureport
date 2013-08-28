f6736f564caf380f3e117db18e6227e7c5feff56
Carlos Lopes <Carlos@Carlos-MacBook-Pro.local>
2013-08-28 11:02:43 +0300

    [Carlos,MJ] import_csv_locations should be runnable multiple times -- and now it is.

--- ureport/management/commands/import_csv_locations.py
+++ ureport/management/commands/import_csv_locations.py
@@ -24,18 +24,17 @@ def insert_location_types(sheet, reporting_level):
     for i in range(len(sheet.title_row)):
         location_type_name = sheet.title_row[i]
         if i == reporting_level:
-            LocationType.objects.create(name=location_type_name, slug='district')
+            LocationType.objects.get_or_create(name=location_type_name, slug='district')
         else:
-            LocationType.objects.create(name=location_type_name, slug=location_type_name)
+            LocationType.objects.get_or_create(name=location_type_name, slug=location_type_name)
 
 
 def add_location(loc_name, parent, location_point, type_id=None):
-    Location.objects.create(name=loc_name, tree_parent=parent, type_id=type_id, point=location_point, code=loc_name)
-
+    Location.objects.get_or_create(name=loc_name, tree_parent=parent, type_id=type_id, point=location_point, code=loc_name)
 
 def insert_all_locations_into_db(sheet, reporting_level, root_node_name):
-    Location.objects.create(name=root_node_name)
-    location_point = Point.objects.create(latitude=0, longitude=0)
+    Location.objects.get_or_create(name=root_node_name)
+    location_point, created = Point.objects.get_or_create(latitude=0, longitude=0)
 
     while sheet.has_next_column():
         column = sheet.get_next_column()
