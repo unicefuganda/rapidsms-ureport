3c5b0ad7a2b193119f54f99b242fe866b0faca73
Luiza Nunes <luhhsnunes@gmail.com>
2013-09-03 16:42:30 +0300

    [Luiza, Vivi] Adding a time.sleep at poll status tests to investigate the random failure

--- ureport/tests/functional/funct_poll_status.py
+++ ureport/tests/functional/funct_poll_status.py
@@ -1,4 +1,5 @@
 from splinter import Browser
+import time
 from ureport.tests.functional.create_poll_for_tests import start_poll_queues_messages_in_table
 from ureport.tests.functional.splinter_wrapper import SplinterTestCase
 from ureport.tests.functional.take_screenshot import take_screenshot_on_failure
@@ -25,6 +26,7 @@ class PollStatusTest(SplinterTestCase):
         self.go_to_poll_status_page(self.poll.id)
         # self.assertEqual(self.browser.is_element_present_by_id('poll-details'), True)
         # self.assertTrue(str(self.poll.id) in self.browser.find_by_id("poll-details").first.text)
+        time.sleep(10)
         self.assertEqual(self.browser.find_by_id('contact-count').text, "2")
         self.assertEqual(self.browser.find_by_id('category-count').text, "3")
         self.assertEqual(self.browser.find_by_id('is-yesno').text, "yes")
