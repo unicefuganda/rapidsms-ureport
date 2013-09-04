94bfba2a8533229a7dade124547328f7f7ff56c2
Kenneth Matovu <matovken@gmail.com>
2013-09-04 17:07:23 +0300

    no need to put time limit on home page cache

--- ureport/views/dashboard_views.py
+++ ureport/views/dashboard_views.py
@@ -523,6 +523,6 @@ def home(request):
         rendered = cache.get('cached_home')
     else:
         rendered = render_to_string('ureport/home.html', context_instance=RequestContext(request))
-        cache.set('cached_home', rendered, 1200)
+        cache.set('cached_home', rendered)
         cache.set('latest_pk', latest)
     return HttpResponse(rendered)
