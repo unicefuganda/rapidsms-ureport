1235296f7c845eda4420a657958e38b25c3a959d
wanderanimrod <wanderanimrod@gmail.com>
2013-09-06 11:12:17 +0300

    [nim, Carlos] Parameterising test server url for functional tests so we can run them on a server external to ci

--- ureport/tests/functional/splinter_wrapper.py
+++ ureport/tests/functional/splinter_wrapper.py
@@ -1,20 +1,19 @@
 import datetime
 from django.contrib.auth.models import User
-from django_liveserver.testcases import LiveServerTestCase
+from django.test import TransactionTestCase
+import settings
 
 
-class SplinterTestCase(LiveServerTestCase):
+class SplinterTestCase(TransactionTestCase):
     def __init__(self, methodName='runTest'):
         super(SplinterTestCase, self).__init__(methodName)
 
     def follow_link(self, text):
         (self.browser.find_element_by_link_text(text)).click()
 
-
     def select_by_text(self, name, text):
         self.browser.find_by_xpath(
             '//select[@name="%s"]/option[normalize-space(.)="%s"]' % (name, text)).first._element.click()
-
 
     def create_and_sign_in_admin(self, username, password, redirect_to):
         User.objects.create_superuser(username, 'admin@test.com', password)
@@ -29,7 +28,7 @@ class SplinterTestCase(LiveServerTestCase):
         self.browser.find_by_css("input[type=submit]").first.click()
 
     def open(self, url):
-        self.browser.visit("%s%s" % (self.live_server_url, url))
+        self.browser.visit("%s%s" % (settings.TEST_SERVER_URL, url))
 
     def wait_for_seconds(self, time_out_in_seconds):
         current_time = datetime.datetime.now()
