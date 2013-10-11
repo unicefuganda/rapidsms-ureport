c7e883b8748181a0caa61f2334b00881de4213be
Luiza Nunes <luhhsnunes@gmail.com>
2013-10-11 07:41:14 +0000

    [Argha, Luiza] All the tests will run on CI now

--- ureport/tests/functional/funct_poll_responses.py
+++ ureport/tests/functional/funct_poll_responses.py
@@ -41,7 +41,6 @@ class PollResponsesTest(unittest.TestCase, PollAssertions):
         PollBase.start_poll(self.browser,self.poll_id)
 
         PollBase.respond_to_the_started_poll("0794339344", "yes")
-        time.sleep(WAIT_TIME_IN_SECONDS)
         SplinterWrapper.open('/polls/%s/report/' % self.poll_id)
         self.assert_that_question_is(self.question)
         self.assert_the_number_of_participants_of_the_poll_is(1)
@@ -49,19 +48,19 @@ class PollResponsesTest(unittest.TestCase, PollAssertions):
         self.assert_that_response_location_is("Kasese")
         self.assert_that_number_of_responses_is(1)
 
-    # def test_that_a_poll_response_can_be_reassigned_to_another_poll(self):
-    #     second_poll_id = PollBase.create_poll(self.browser,name='Second Poll',type="Yes/No Question",question="Is the first poll working?",group="groupFT")
-    #     PollBase.start_poll(self.browser,second_poll_id)
-    #
-    #     PollBase.respond_to_the_started_poll("0794339344", "yes")
-    #     PollBase.close_poll(second_poll_id)
-    #
-    #     PollBase.reassign_poll_response(second_poll_id,self.poll_id)
-    #     time.sleep(2) #Takes a time for a poll to be reassigned
-    #     PollBase.start_poll(self.browser,self.poll_id)
-    #
-    #     SplinterWrapper.open('/polls/%s/report/' % self.poll_id)
-    #     self.assert_that_number_of_responses_is(1)
+    def test_that_a_poll_response_can_be_reassigned_to_another_poll(self):
+        second_poll_id = PollBase.create_poll(self.browser,name='Second Poll',type="Yes/No Question",question="Is the first poll working?",group="groupFT")
+        PollBase.start_poll(self.browser,second_poll_id)
+
+        PollBase.respond_to_the_started_poll("0794339344", "yes")
+        PollBase.close_poll(second_poll_id)
+
+        PollBase.reassign_poll_response(second_poll_id,self.poll_id)
+        time.sleep(2) #Takes a time for a poll to be reassigned
+        PollBase.start_poll(self.browser,self.poll_id)
+
+        SplinterWrapper.open('/polls/%s/report/' % self.poll_id)
+        self.assert_that_number_of_responses_is(1)
 
     def test_that_a_response_can_be_replied_to_an_ureporter(self):
         message = "Hello"
